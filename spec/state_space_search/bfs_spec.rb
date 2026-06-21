bfdecee2e805f088ea757bbe53bf3e069b8c5b07
no-la <nola.general.6579@gmail.com>
2026-06-21 21:47:01 +0900

    Add failing BFS reachability spec

---
+++ spec/state_space_search/bfs_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/state_space_search'
+
+RSpec.describe StateSpaceSearch::BFS do
+  it '1から3へ+1ずつのBFSでゴールに到達できる' do
+    result = described_class.search(
+      start: 1,
+      goal: ->(state) { state == 3 },
+      transitions: ->(state) { [state + 1] }
+    )
+
+    expect(result).to be_reachable
+  end
+end
