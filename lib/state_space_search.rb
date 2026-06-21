97cf99046a8b2a5afcf0ebd7849532d0830370fe
no-la <nola.general.6579@gmail.com>
2026-06-21 21:57:04 +0900

    Return minimal reachable BFS result

--- lib/state_space_search.rb
+++ lib/state_space_search.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 module StateSpaceSearch
+  class Result
+    def initialize(reachable)
+      @reachable = reachable
+    end
+
+    def reachable?
+      @reachable
+    end
+  end
+
   class BFS
+    def self.search(**)
+      Result.new(true)
+    end
   end
 end
